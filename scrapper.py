ded4488a3d2971388792990d8a76606f753ebdfa
Felipe C <fcabeza001@gmail.com>
2021-04-29 23:14:04 -0500

    search button ready

--- scrapper.py
+++ scrapper.py
@@ -2,15 +2,41 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.action_chains import ActionChains
 
 # Initial Configurations
 
 URL = 'https://sia.unal.edu.co/ServiciosApp/facespublico/public/servicioPublico.jsf?taskflowId=task-flow-AC_CatalogoAsignaturas'
 driver = webdriver.Firefox()
 
+# helpers and functions
 
 
+def waiting_for_cursor(function):
+    def wrapper(*args, **kwargs):
+        select_element = function(*args, **kwargs)
+        # Waiting for the cursor to be available
+        WebDriverWait(driver, 20).until(lambda cursor: driver.find_element_by_tag_name(
+            "body").value_of_css_property("cursor") != "wait")
+        return select_element
+    return wrapper
+
+
+@waiting_for_cursor
+def selecting_element_by_id(id_value: str, index_value: int):
+    """
+    Select the current value on the dropdown element. 
+    """
+    select_element = driver.find_element(By.ID, id_value)
+    return Select(select_element).select_by_index(index_value)
+
+
+@waiting_for_cursor
+def click_button(xpath_value: str):
+    """
+    Click a button based on the xpath.
+    """
+    button = driver.find_element_by_xpath(xpath_value)
+    return button.click()
 
 
 # Selecting search parameters
@@ -39,14 +65,7 @@ Select 1 => Amazonia,
 faculty = selecting_element_by_id("pt1:r1:0:soc2::content", 1)
 syllabus = selecting_element_by_id("pt1:r1:0:soc3::content", 1)
 
-# Optional Parameters
-# major_type = selecting_element_by_id("pt1:r1:0:soc4::content", 1)
-
-
-
-# driver.quit()
-
-# Start scrapping
+click_button("//div[@id='pt1:r1:0:cb1']")
 
 
 
@@ -56,26 +75,3 @@ syllabus = selecting_element_by_id("pt1:r1:0:soc3::content", 1)
 
 
 
-
-
-
-
-# helpers and functions 
-
-def waiting_for_cursor(function):
-    def wrapper(*args, **kwargs):
-        select_element = function(*args, **kwargs)
-        # Waiting for the cursor to be available
-        WebDriverWait(driver, 20).until(lambda cursor: driver.find_element_by_tag_name(
-            "body").value_of_css_property("cursor") != "wait")
-        return select_element
-    return wrapper
-
-
-@waiting_for_cursor
-def selecting_element_by_id(id_value: str, index_value: int) -> Select:
-    """
-    Select the current value on the dropdown element. 
-    """
-    select_element = driver.find_element(By.ID, id_value)
-    return Select(select_element).select_by_index(index_value)
